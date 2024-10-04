c64cc5cc9e5fd43e1a4d4551eae0866d8ebc53bd
Tanisi90 <69205416+Tanisi90@users.noreply.github.com>
2024-10-04 10:50:33 -0700

    Update SeleniumTest.java

--- src/test/java/com/revature/SeleniumTest.java
+++ src/test/java/com/revature/SeleniumTest.java
@@ -26,6 +26,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumTest {
 
